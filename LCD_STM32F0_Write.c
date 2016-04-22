46709e3fd203133772cbb7526cc11f79aabb0ff0
SovaidKhan <sovaidkhan@gmail.com>
2016-04-22 19:05:31 +0200

    LCD header files to write all characters

---
+++ LCD_STM32F0_Write.c
@@ -0,0 +1,8 @@
+#include "LCD_STM32F0_Write.h"
+
+void LCD_write(void)
+{
+
+
+
+}
---
+++ LCD_STM32F0_Write.h
@@ -0,0 +1,6 @@
+#ifndef LCD_STM32F0_Write   /* Include guard */
+#define LCD_STM32F0_Write
+
+int foo(int x);  /* An example function declaration */
+
+#endif // LCD_STM32F0_Write
